da5309b217305e5ca2509108419d8d2c8284c0d3
Robert Szymański <biuro@staircase.pl>
2014-07-18 11:08:54 +0200

    Boolka

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Golden Ratio Grid</title>
+        <link href="style.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="phi">
+            <div class="alpha">
+                <div class="phi">
+                    <div class="alpha">
+
+                    </div>
+                    <div class="omega">
+                        <div class="row">
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                            <div class="one columns"></div>
+                        </div>   
+                    </div>
+                </div>
+            </div>
+            <div class="omega">
+                <div class="row">
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                    <div class="one columns"></div>
+                </div>                
+            </div>
+        </div>
+        <div class="phi">
+            <div class="alpha">
+                <div class="row">
+                    <div class="three columns"></div>
+                    <div class="three columns"></div>
+                    <div class="three columns"></div>
+                    <div class="three columns"></div>
+                    <div class="one columns"></div>
+                </div> 
+            </div>
+            <div class="omega">
+                <div class="row">
+                    <div class="two columns"></div>
+                    <div class="two columns"></div>
+                    <div class="two columns"></div>
+                    <div class="two columns"></div>
+                    <div class="two columns"></div>
+                    <div class="three columns"></div>
+                </div>  
+            </div>
+        </div>
+        <div class="phi">
+            <div class="alpha">
+                <div class="row">
+                    <div class="four columns"></div>
+                    <div class="five columns"></div>
+                    <div class="four columns"></div>
+                </div> 
+            </div>
+            <div class="omega">
+                <div class="row">
+                    <div class="five columns"></div>
+                    <div class="six columns"></div>
+                    <div class="two columns"></div>
+                </div>  
+            </div>
+        </div>
+        <div class="phi">
+            <div class="alpha">
+                <div class="row">
+                    <div class="six columns"></div>
+                    <div class="seven columns"></div>
+                </div> 
+            </div>
+            <div class="omega">
+                <div class="row">
+                    <div class="eight columns"></div>
+                    <div class="five columns"></div>
+                </div>  
+            </div>
+        </div>
+        <div class="phi">
+            <div class="alpha">
+                <div class="row">
+                    <div class="nine columns"></div>
+                    <div class="one columns"></div>
+                    <div class="three columns"></div>
+                </div> 
+            </div>
+            <div class="omega">
+                <div class="row">
+                    <div class="ten columns"></div>
+                    <div class="three columns"></div>
+                </div>  
+            </div>
+        </div>
+        <div class="phi">
+            <div class="alpha">
+                <div class="row">
+                    <div class="eleven columns"></div>
+                    <div class="two columns"></div>
+                </div> 
+            </div>
+            <div class="omega">
+                <div class="row">
+                    <div class="twelve columns"></div>
+                    <div class="one columns"></div>
+                </div>  
+            </div>
+        </div>
+    </body>
+</html>
